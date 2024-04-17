7fe9d9988972d1c50436b1d30dc5f86e7ba8496b
= <kr6.inge@gmail.com>
2024-04-17 16:09:07 -0500

    end apply

--- code.js
+++ code.js
@@ -1,21 +1,18 @@
-
-const videoElement = document.getElementById('input_video');
-const canvasElement = document.getElementById('my_canvas');
-const canvasCtx = canvasElement.getContext('2d');
-const textConsole = document.getElementById('textConsole');
+const videoElement = document.getElementById("input_video");
+const canvasElement = document.getElementById("my_canvas");
+const canvasCtx = canvasElement.getContext("2d");
+const textConsole = document.getElementById("textConsole");
 textConsole.innerHTML = "Jump vision";
 //get width and height display properties
 var widthDisplay = canvasElement.width;
 var heightDisplay = canvasElement.height;
-var positionButtonFinal = [100,50];
-
+var positionButtonFinal = [100, 50];
 
 //sound
-var soundPoint = new Audio('sound/point.mp3');
-var errorPoint = new Audio('sound/error.mp3');
+var soundPoint = new Audio("sound/point.mp3");
+var errorPoint = new Audio("sound/error.mp3");
 
-
-//position obstacle 
+//position obstacle
 var xs = [widthDisplay];
 
 //positionInitialObstacle
@@ -32,48 +29,70 @@ var pointMore = 0;
 var pointLess = 0;
 var updateConsole = false;
 
-//true parameters 
+//true parameters
 var trueParameters = false;
 
-
 function moveLine(x, y) {
-    //init path
-    canvasCtx.beginPath();
-    //move path
-    canvasCtx.moveTo(10, 10);
-    //canvas draw line
-    canvasCtx.lineTo(x, y);
-    //canvas draw
-    canvasCtx.stroke();
-}
-
-function evalueCollisiont(canvas,points){
-  canvasCtx.strokeStyle = "red";
-  canvasCtx.lineWidth = 3;
-  canvas.fillStyle = 'red';
   //init path
   canvasCtx.beginPath();
-  canvas.arc(points[31].x*widthDisplay, points[31].y*heightDisplay,10,0,2*Math.PI);
+  //move path
+  canvasCtx.moveTo(10, 10);
+  //canvas draw line
+  canvasCtx.lineTo(x, y);
   //canvas draw
   canvasCtx.stroke();
-  if(trueParameters){
-    if(points[31].x*widthDisplay>xs[0] && points[31].x*widthDisplay<xs[0]+widtObstacle || 
-  points[32].x*widthDisplay>xs[0] && points[32].x*widthDisplay<xs[0]+widtObstacle){
+}
+
+function evalueCollision(canvas, points) {
+  canvasCtx.strokeStyle = "red";
+  canvasCtx.lineWidth = 3;
+  canvas.fillStyle = "red";
+  //init path
+  canvasCtx.beginPath();
+  canvas.arc(
+    points[31].x * widthDisplay,
+    points[31].y * heightDisplay,
+    10,
+    0,
+    2 * Math.PI
+  );
+  //canvas draw
+  canvasCtx.stroke();
+  if (trueParameters) {
+    //evalue position in x
+    if (
+      (points[31].x * widthDisplay > xs[0] &&
+        points[31].x * widthDisplay < xs[0] + widtObstacle) ||
+      (points[32].x * widthDisplay > xs[0] &&
+        points[32].x * widthDisplay < xs[0] + widtObstacle)
+    ) {
       updateConsole = true;
-        if(points[31].y*heightDisplay>heightObstacleInitial-heightObstacleLarge && points[32].y*heightDisplay>heightObstacleInitial-heightObstacleLarge){
-          wasCollision = true;
-        }
-    }else{
-      if(points[31].x*widthDisplay>xs[0] && points[31].x*widthDisplay>xs[0]+widtObstacle && updateConsole){
-        updateConsole = false
-        if(wasCollision){
-          pointLess +=1;
-          textConsole.innerHTML = "Punto: "+pointMore+ " Colisión: "+ pointLess;
+      //evalue position in y
+      if (
+        points[31].y * heightDisplay >
+          heightObstacleInitial - heightObstacleLarge &&
+        points[32].y * heightDisplay >
+          heightObstacleInitial - heightObstacleLarge
+      ) {
+        wasCollision = true;
+      }
+    } else {
+      if (
+        points[31].x * widthDisplay > xs[0] &&
+        points[31].x * widthDisplay > xs[0] + widtObstacle &&
+        updateConsole
+      ) {
+        updateConsole = false;
+        if (wasCollision) {
+          pointLess += 1;
+          textConsole.innerHTML =
+            "Punto: " + pointMore + " Colisión: " + pointLess;
           errorPoint.play();
-        }else{
+        } else {
           soundPoint.play();
-          pointMore +=1;
-          textConsole.innerHTML = "Punto: "+pointMore+ " Colisión: "+ pointLess;
+          pointMore += 1;
+          textConsole.innerHTML =
+            "Punto: " + pointMore + " Colisión: " + pointLess;
         }
       }
     }
@@ -82,204 +101,168 @@ function evalueCollisiont(canvas,points){
 
 /**
  * Start game with parameters
- * @param {*} canvasCtx 
- * @param {*} points 
+ * @param {*} canvasCtx
+ * @param {*} points
  */
-function clickStart(canvasCtx,points) {
-    if (points[20].x*widthDisplay>0 && 
-      points[20].y*heightDisplay>0 && 
-      points[20].x*widthDisplay<positionButtonFinal[0] && 
-      points[20].y*heightDisplay<positionButtonFinal[1]){
-        console.log("clickStart");
-        trueParameters = true;
-      }
+function clickStart(canvasCtx, points) {
+  if (
+    points[20].x * widthDisplay > 0 &&
+    points[20].y * heightDisplay > 0 &&
+    points[20].x * widthDisplay < positionButtonFinal[0] &&
+    points[20].y * heightDisplay < positionButtonFinal[1]
+  ) {
+    console.log("clickStart");
+    trueParameters = true;
+  }
 }
-
-
 
 /**
  * Function draw button for init parameters
- * @param {*} canvasCtx 
- * @param {*} points 
+ * @param {*} canvasCtx
+ * @param {*} points
  */
-function buttonStart(canvasCtx,points) {
+function buttonStart(canvasCtx, points) {
   canvasCtx.strokeStyle = "black";
   canvasCtx.lineWidth = 1;
-  canvasCtx.fillStyle = 'green';
+  canvasCtx.fillStyle = "green";
   canvasCtx.beginPath();
-  canvasCtx.rect(0,0,positionButtonFinal[0],positionButtonFinal[1]);
+  canvasCtx.rect(0, 0, positionButtonFinal[0], positionButtonFinal[1]);
   canvasCtx.fill();
-  canvasCtx.fillStyle = 'white';
+  canvasCtx.fillStyle = "white";
   canvasCtx.font = "24px Arial";
-  canvasCtx.fillText("Start",positionButtonFinal[0]/4,positionButtonFinal[1]/2);
+  canvasCtx.fillText(
+    "Start",
+    positionButtonFinal[0] / 4,
+    positionButtonFinal[1] / 2
+  );
   canvasCtx.stroke();
 }
 
-
 /**
- * 
- * @param {*} canvasCtx 
- * @param {*} points
- * Creaate a obstacle for simulation 
+ *
+ * @param {object} canvasCtx
+ * @param {object[]} points
+ * Creaate a obstacle for simulation
  */
 function drawObstacle(canvasCtx, points) {
   canvasCtx.strokeStyle = "black";
   canvasCtx.lineWidth = 3;
-  canvasCtx.fillStyle = 'red';
+  canvasCtx.fillStyle = "red";
   canvasCtx.beginPath();
   //reduce position display
   xs[0] -= 10;
-  if(trueParameters == false){
+  if (trueParameters == false) {
     //width obstacle is
-    widtObstacle = widthDisplay/8;
-    heightObstacleLarge = Math.abs((points[30].x - points[29].x)*widthDisplay);
-    heightObstacleInitial = points[31].y*heightDisplay;
+    widtObstacle = widthDisplay / 8;
+    heightObstacleLarge = Math.abs(
+      (points[30].x - points[29].x) * widthDisplay
+    );
+    heightObstacleInitial = points[31].y * heightDisplay;
   }
-  
+
   //create obstacles
-  canvasCtx.rect(xs[0], heightObstacleInitial, widtObstacle, -heightObstacleLarge);
+  canvasCtx.rect(
+    xs[0],
+    heightObstacleInitial,
+    widtObstacle,
+    -heightObstacleLarge
+  );
   canvasCtx.fill();
-//canvasCtx.fill();
+  //canvasCtx.fill();
   canvasCtx.stroke();
-  //restar box
-  if(xs[0]<0){
-    
+  //restart box
+  if (xs[0] < 0) {
     xs[0] = widthDisplay;
     //reinit point more
     wasCollision = false;
   }
 }
 
-
 //on results
 function onResults(results) {
-    if (!results.poseLandmarks) {
-      //grid.updateLandmarks([]);
-      return;
-    }
-
-    
-
-    canvasCtx.save(); // Guarda el estado actual del contexto de dibujo del canvas
-
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height); // Limpia todo el canvas
-    
-    // Dibuja la máscara de segmentación para crear un fondo negro detrás de la persona
-    canvasCtx.drawImage(results.segmentationMask, 0, 0, canvasElement.width, canvasElement.height);
-    
-    canvasCtx.globalCompositeOperation = 'source-in'; // Solo conserva píxeles donde tanto la fuente como el destino son opacos
-    
-    canvasCtx.fillStyle = '#0F00'; // Establece el estilo de relleno para la segmentación.
-    
-    canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height); // Rellena todo el canvas con el color oscuro
-    
-    // Dibuja la imagen original, creando efectivamente un efecto de recorte
-    canvasCtx.globalCompositeOperation = 'destination-atop';
-    
-    canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
-    
-    canvasCtx.globalCompositeOperation = 'source-over'; // Restablece el modo de mezcla predeterminado
-    
-    // Dibuja las conexiones y los puntos de referencia de la pose encima de todo
-    drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {color: '#FFFF', lineWidth: 6});
-    drawLandmarks(canvasCtx, results.poseLandmarks, {color: '#000000', lineWidth: 2});
-    
-    
-    //draw obstacles
-    drawObstacle(canvasCtx, results.poseLandmarks);
-    //button start
-    buttonStart(canvasCtx, results.poseLandmarks);
-    //click start
-    clickStart(canvasCtx, results.poseLandmarks);
-    //evalue collision
-    evalueCollisiont(canvasCtx,results.poseLandmarks);
-
-
-    canvasCtx.restore(); // Restaura el estado guardado del canvas
-    
-
-}
-
-//pose
-const pose = new Pose({locateFile: (file) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
-  }});
-  pose.setOptions({
-    modelComplexity: 1,
-    smoothLandmarks: true,
-    enableSegmentation: true,
-    smoothSegmentation: true,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5
-  });
-  pose.onResults(onResults);
-
-
-if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {  
-    const camera = new Camera(videoElement, {
-        onFrame: async () => {
-            await pose.send({image: videoElement});
-        },
-        width: 500,
-        height: 500,
-        facingMode:'environment'
-      });
-      
-      camera.start();
-}
-
-//const landmarkContainer = document.getElementsByClassName('landmark-grid-container')[0];
-//const grid = new LandmarkGrid(landmarkContainer);
-/*
-function onResults(results) {
   if (!results.poseLandmarks) {
-    grid.updateLandmarks([]);
     return;
   }
 
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(results.segmentationMask, 0, 0,
-                      canvasElement.width, canvasElement.height);
+  canvasCtx.save(); // Guarda el estado actual del contexto de dibujo del canvas
 
-  // Only overwrite existing pixels.
-  canvasCtx.globalCompositeOperation = 'source-in';
-  canvasCtx.fillStyle = '#00FF00';
-  canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height); // Limpia todo el canvas
 
-  // Only overwrite missing pixels.
-  canvasCtx.globalCompositeOperation = 'destination-atop';
+  // Dibuja la máscara de segmentación para crear un fondo negro detrás de la persona
   canvasCtx.drawImage(
-      results.image, 0, 0, canvasElement.width, canvasElement.height);
+    results.segmentationMask,
+    0,
+    0,
+    canvasElement.width,
+    canvasElement.height
+  );
 
-  canvasCtx.globalCompositeOperation = 'source-over';
-  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
-                 {color: '#00FF00', lineWidth: 4});
-  drawLandmarks(canvasCtx, results.poseLandmarks,
-                {color: '#FF0000', lineWidth: 2});
-  canvasCtx.restore();
+  canvasCtx.globalCompositeOperation = "source-in"; // Solo conserva píxeles donde tanto la fuente como el destino son opacos
 
-  grid.updateLandmarks(results.poseWorldLandmarks);
+  canvasCtx.fillStyle = "#0F00"; // Establece el estilo de relleno para la segmentación.
+
+  canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height); // Rellena todo el canvas con el color oscuro
+
+  // Dibuja la imagen original, creando efectivamente un efecto de recorte
+  canvasCtx.globalCompositeOperation = "destination-atop";
+
+  canvasCtx.drawImage(
+    results.image,
+    0,
+    0,
+    canvasElement.width,
+    canvasElement.height
+  );
+
+  canvasCtx.globalCompositeOperation = "source-over"; // Restablece el modo de mezcla predeterminado
+
+  // Dibuja las conexiones y los puntos de referencia de la pose encima de todo
+  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, {
+    color: "#FFFF",
+    lineWidth: 6,
+  });
+  drawLandmarks(canvasCtx, results.poseLandmarks, {
+    color: "#000000",
+    lineWidth: 2,
+  });
+
+  //draw obstacles
+  drawObstacle(canvasCtx, results.poseLandmarks);
+  //button start
+  buttonStart(canvasCtx, results.poseLandmarks);
+  //click start
+  clickStart(canvasCtx, results.poseLandmarks);
+  //evalue collision
+  evalueCollision(canvasCtx, results.poseLandmarks);
+
+  canvasCtx.restore(); // Restaura el estado guardado del canvas
 }
 
-const pose = new Pose({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
-}});
+//pose
+const pose = new Pose({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+  },
+});
 pose.setOptions({
   modelComplexity: 1,
   smoothLandmarks: true,
   enableSegmentation: true,
   smoothSegmentation: true,
   minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
+  minTrackingConfidence: 0.5,
 });
 pose.onResults(onResults);
 
-const camera = new Camera(videoElement, {
-  onFrame: async () => {
-    await pose.send({image: videoElement});
-  },
-  width: 1280,
-  height: 720
-});
-camera.start();*/
+if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices) {
+  const camera = new Camera(videoElement, {
+    onFrame: async () => {
+      await pose.send({ image: videoElement });
+    },
+    width: 500,
+    height: 500,
+    facingMode: "environment",
+  });
+
+  camera.start();
+}
